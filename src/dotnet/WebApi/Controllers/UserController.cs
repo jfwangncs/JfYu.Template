68f22cadc282e7c46d60de2ac8d5f001e1855424
Jinfeng Wang <jinfeng.wang@razer.com>
2026-03-19 11:05:57 +0800

    1

--- src/dotnet/WebApi/Controllers/UserController.cs
+++ src/dotnet/WebApi/Controllers/UserController.cs
@@ -14,68 +14,54 @@ using WebApi.Services.Interfaces;
 
 namespace WebApi.Controllers
 {
-  [ApiController]
-  [Route("api/[controller]")]
-  [Authorize]
-  public class UserController(IUserService userService, ICurrentUser currentUser) : CustomController
-  {
-    private readonly IUserService _userService = userService;
-    private readonly ICurrentUser _currentUser = currentUser;
-
-    [HttpGet]
-    [Authorize(Roles = "Admin")]
-    public async Task<IActionResult> GetAllAsync([FromQuery] QueryRequest query)
-    {
-      var result = await _userService.GetPagedAsync(query);
-      return Ok(result);
-    }
-
-    [HttpGet("{id}")]
-    [Authorize(Roles = "Admin")]
-    public async Task<IActionResult> GetByIdAsync(int id)
-    {
-      var user = await _userService.GetOneAsync(q => q.Id.Equals(id));
-      if (user == null)
-        return BadRequest(ErrorCode.UserNotFound);
-      return Ok(user);
-    }
-
-    [HttpPost]
-    [Authorize(Roles = "Admin")]
-    public async Task<IActionResult> CreateAsync([FromBody][Required] CreateUserRequest request)
-    {
-      var exist = await _userService.GetOneAsync(q => q.UserName == request.UserName);
-      if (exist != null)
-        return BadRequest(ErrorCode.DuplicateUser);
-
-      var user = request.Adapt<User>();
-      user.Password = BCrypt.Net.BCrypt.HashPassword(request.Password);
-      await _userService.AddAsync(user);
-      return Ok(user);
-    }
-
-    [HttpPut("{id}")]
-    [Authorize(Roles = "Admin")]
-    public async Task<IActionResult> UpdateAsync(int id, [FromBody][Required] UpdateUserRequest request)
-    {
-      var user = await _userService.GetOneAsync(q => q.Id.Equals(id));
-      if (user == null)
-        return BadRequest(ErrorCode.UserNotFound);
-
-      if (await _userService.UpdateAsync(request.Adapt(user)) > 0)
-        return Ok(user.Adapt<UserResponse>());
-      else
-        return BadRequest(ErrorCode.OperationFailed);
-    }
-
-    [HttpGet("info")]
+    [ApiController]
+    [Route("api/[controller]")]
     [Authorize]
-    public async Task<IActionResult> GetUserInfo()
+    public class UserController(IUserService userService, ICurrentUser currentUser) : CustomController
     {
-      var user = await _userService.GetOneAsync(q => q.Id.Equals(_currentUser.Id));
-      if (user == null)
-        return BadRequest(ErrorCode.UserNotFound);
-      return Ok(user.Adapt<UserResponse>());
+        private readonly IUserService _userService = userService;
+        private readonly ICurrentUser _currentUser = currentUser;
+
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> GetAllAsync([FromQuery] QueryRequest query)
+        {
+            var result = await _userService.GetPagedAsync(query);
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> GetByIdAsync(int id)
+        {
+            var user = await _userService.GetOneAsync(q => q.Id.Equals(id));
+            if (user == null)
+                return BadRequest(ErrorCode.UserNotFound);
+            return Ok(user);
+        }
+
+        [HttpPut("{id}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> UpdateAsync(int id, [FromBody][Required] UpdateUserRequest request)
+        {
+            var user = await _userService.GetOneAsync(q => q.Id.Equals(id));
+            if (user == null)
+                return BadRequest(ErrorCode.UserNotFound);
+
+            if (await _userService.UpdateAsync(request.Adapt(user)) > 0)
+                return Ok(user.Adapt<UserResponse>());
+            else
+                return BadRequest(ErrorCode.OperationFailed);
+        }
+
+        [HttpGet("info")]
+        [Authorize]
+        public async Task<IActionResult> GetUserInfo()
+        {
+            var user = await _userService.GetOneAsync(q => q.Id.Equals(_currentUser.Id));
+            if (user == null)
+                return BadRequest(ErrorCode.UserNotFound);
+            return Ok(user.Adapt<UserResponse>());
+        }
     }
-  }
 }
